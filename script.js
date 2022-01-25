dd5925b4a91a6bd022251432bb32aeb2754e66e1
mobak88 <mostafa0401@ymail.com>
2022-01-25 14:09:30 +0100

    Adding random fortune logic

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ const chrystallBallImg = document.getElementById('chrystal-ball');
 const userNameSubmitBtn = document.querySelector('.user-name__submit');
 const fortuneCardContainer = document.querySelector('.card__container');
 const userFortune = document.querySelector('.card__paragraph');
+const userName = document.querySelector('.user-name__input').value;
 
 const userNameContainer = document.querySelector(
     '.user-name__container--hidden'
 );
 
-const fortuneOne = `Fortune 1`;
-const fortuneTwo = `Fortune 2`;
-const fortuneThree = `Fortune 3`;
-const fortuneFour = `Fortune 4`;
-const fortuneFive = `Fortune 5`;
+const fortuneOne = `blir rik`;
+const fortuneTwo = `skal ut på en reise`;
+const fortuneThree = `får et kjeledyr`;
+const fortuneFour = `vinner lotto`;
+const fortuneFive = `blir lykkelig`;
 
 let randomNumber = Math.floor((Math.random() * 5) + 1);
 
@@ -21,20 +22,20 @@ const showUserNameInput = () => {
 
 const showFortune = () => {
     fortuneCardContainer.classList.remove('card__container--didplay-none');
-    if (randomNumber = 1) {
-        userFortune.innerHTML = fortuneOne;
+    if (randomNumber === 1) {
+        userFortune.innerHTML = `${userName} ${fortuneOne}`;
     }
-    if (randomNumber = 2) {
-        userFortune.innerHTML = fortuneTwo;
+    if (randomNumber === 2) {
+        userFortune.innerHTML = `${userName} ${fortuneTwo}`;
     }
-    if (randomNumber = 3) {
-        userFortune.innerHTML = fortuneThree;
+    if (randomNumber === 3) {
+        userFortune.innerHTML = `${userName} ${fortuneThree}`;
     }
-    if (randomNumber = 4) {
-        userFortune.innerHTML = fortuneFour;
+    if (randomNumber === 4) {
+        userFortune.innerHTML = `${userName} ${fortuneFour}`;
     }
-    if (randomNumber = 5) {
-        userFortune.innerHTML = fortuneFive;
+    if (randomNumber === 5) {
+        userFortune.innerHTML = `${userName} ${fortuneFive}`;
     }
     console.log(randomNumber);
 };
@@ -44,4 +45,6 @@ chrystallBallImg.addEventListener('click', showUserNameInput);
 userNameSubmitBtn.addEventListener('click', (e) => {
     e.preventDefault();
     showFortune();
+    randomNumber = Math.floor((Math.random() * 5) + 1);
+    console.log(userName);
 });
