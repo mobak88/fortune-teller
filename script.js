d7e20367a624c5fc42c25e7ecf123eaa126f373e
mobak88 <mostafa0401@ymail.com>
2022-01-25 15:21:50 +0100

    Finishing assignment

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const userNameSubmitBtn = document.querySelector('.user-name__submit');
 const fortuneCardContainer = document.querySelector('.card__container');
 const userFortune = document.querySelector('.card__paragraph');
 const userName = document.querySelector('.user-name__input');
+const card = document.querySelector('.card');
 
 const userNameContainer = document.querySelector(
     '.user-name__container--hidden'
@@ -16,34 +17,39 @@ const fortuneFive = `blir lykkelig`;
 
 let randomNumber = Math.floor((Math.random() * 5) + 1);
 
-/* const showUserNameInput = () => {
+const showUserNameInput = () => {
     userNameContainer.classList.remove('user-name__container--hidden');
-}; */
+};
 
 const showFortune = () => {
     fortuneCardContainer.classList.remove('card__container--didplay-none');
+
     if (randomNumber === 1) {
         userFortune.innerHTML = `${userName.value} ${fortuneOne}`;
+        card.style.backgroundColor = 'rgb(247, 225, 225)';
     }
     if (randomNumber === 2) {
         userFortune.innerHTML = `${userName.value} ${fortuneTwo}`;
+        card.style.backgroundColor = 'rgb(193, 250, 198)';
     }
     if (randomNumber === 3) {
         userFortune.innerHTML = `${userName.value} ${fortuneThree}`;
+        card.style.backgroundColor = 'rgb(182, 183, 196)';
     }
     if (randomNumber === 4) {
         userFortune.innerHTML = `${userName.value} ${fortuneFour}`;
+        card.style.backgroundColor = 'gb(145, 163, 221)';
     }
     if (randomNumber === 5) {
         userFortune.innerHTML = `${userName.value} ${fortuneFive}`;
+        card.style.backgroundColor = 'rgb(206, 145, 221)';
     }
 };
 
-// chrystallBallImg.addEventListener('click', showUserNameInput);
+chrystallBallImg.addEventListener('click', showUserNameInput);
 
 userNameSubmitBtn.addEventListener('click', (e) => {
     e.preventDefault();
     showFortune();
     randomNumber = Math.floor((Math.random() * 5) + 1);
-    console.log(userName);
 });
